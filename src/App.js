25aa5f8531b1855fc1ba40fa344427e709057b2d
Sabbir Shuvo <Shabbir Ahmed Shuvo>
2022-05-06 15:20:19 +0600

    adding

--- src/App.js
+++ src/App.js
@@ -4,20 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2 className='bg-primary'>I am here</h2>
     </div>
   );
 }
